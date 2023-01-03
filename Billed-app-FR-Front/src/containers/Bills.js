144bdb577c8195fddbed1c4bb7b982e84ae1e7be
StevenC <steven69700@gmail.com>
2023-01-03 11:19:36 +0100

    ajout de commentaire sur Bills.js et NewBills.js

--- Billed-app-FR-Front/src/containers/Bills.js
+++ Billed-app-FR-Front/src/containers/Bills.js
@@ -3,13 +3,18 @@ import { formatDate, formatStatus } from "../app/format.js"
 import Logout from "./Logout.js"
 
 export default class {
+  //constructor de l'objet Bills
   constructor({ document, onNavigate, store, localStorage }) {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
+    //selection dans le Dom du bouton new Bill
     const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
+    //si le bouton new Bill existe on lui ajoute un listener qui appel la fonction handleClickNewBill
     if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
+    //selection dans le Dom du bouton icon Eye
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
+    // le bouton icon Eye existe (pour tout les bouton) on lui ajoute un listener qui appel la fonction handleClickIconEye
     if (iconEye) iconEye.forEach(icon => {
       icon.addEventListener('click', () => this.handleClickIconEye(icon))
     })
@@ -17,11 +22,14 @@ export default class {
   }
 
   handleClickNewBill = () => {
+    //accède à la page new Bills
     this.onNavigate(ROUTES_PATH['NewBill'])
   }
 
   handleClickIconEye = (icon) => {
+    //recupère l'attribue Data bill url de l'icon passer en paramètre
     const billUrl = icon.getAttribute("data-bill-url")
+    // raoute la classe show à la modal et renseigne la bonne image dans la modale
     const imgWidth = Math.floor($('#modaleFile').width() * 0.5)
     $('#modaleFile').find(".modal-body").html(`<div style='text-align: center;' class="bill-proof-container"><img width=${imgWidth} src=${billUrl} alt="Bill" /></div>`)
     $('#modaleFile').modal('show')
--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -2,27 +2,44 @@ import { ROUTES_PATH } from '../constants/routes.js'
 import Logout from "./Logout.js"
 
 export default class NewBill {
+  //constructor de l'objet newBill
   constructor({ document, onNavigate, store, localStorage }) {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
+    //recupere le formulaire de new Bill dans le Dom 
     const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
+    //ajout un listener sur le bouton envoyer du formulaire et appel la fonction handleSubmit
     formNewBill.addEventListener("submit", this.handleSubmit)
+    //recupère l'input dont le data-testid est "file" dans le DOM
     const file = this.document.querySelector(`input[data-testid="file"]`)
+     //ajout un listener sur l'input  et appel la fonction handleChangeFile
     file.addEventListener("change", this.handleChangeFile)
+    //déclare fileUrl et lui donne la valeur null
     this.fileUrl = null
+    //déclare fileName et lui donne la valeur null
     this.fileName = null
+    //déclare billId et lui donne la valeur null
     this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
+  //fonction handleChangeFile (qui est appeler sur le input "file")
   handleChangeFile = e => {
+    //annulation du comportement par défaut du bouton 
     e.preventDefault()
+    //recupère l'input dont le data-testid est "file" dans le DOM    
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]    
+    //recupération des information concernant le fichier envoyer via l'input
     const filePath = e.target.value.split(/\\/g)    
+    // récupération du nom du fichier
     const fileName = filePath[filePath.length-1]    
+    // déclaration de l'objet formData
     const formData = new FormData()    
+    //test du format du fichier passer en paramêtre (si pas valide on supprime le fichier)
     if (fileName.split(".").pop() != ("jpg" || "jpeg" || "png")) {this.document.querySelector(`input[data-testid="file"]`).value = []}
+    // récupération de l'email de l'utilisateur
     const email = JSON.parse(localStorage.getItem("user")).email
+    //ajout de l'attribue file et email à l'objet formData
     formData.append('file', file)
     formData.append('email', email)
 
@@ -41,10 +58,15 @@ export default class NewBill {
         this.fileName = fileName
       }).catch(error => console.error(error))
   }
+
   handleSubmit = e => {
+    //annulation du comportement par défaut du bouton 
     e.preventDefault()
+    //test dans la console
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
+    // récupération de l'email de l'utilisateur
     const email = JSON.parse(localStorage.getItem("user")).email
+    //création du bill avec les donnée contenue dans le formulaire
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
@@ -58,7 +80,9 @@ export default class NewBill {
       fileName: this.fileName,
       status: 'pending'
     }
+    // utilisation de la fonction qui ajoute le nouveau bill dans la base de donnée
     this.updateBill(bill)
+    // utilise le routeur pour aller sur la page des Bills.
     this.onNavigate(ROUTES_PATH['Bills'])
   }
 
